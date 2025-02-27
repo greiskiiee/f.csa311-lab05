4a16e6907473b8e7f3e3c8d7dcdac2a1092906fa
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-02-27 20:54:28 +0800

    IntQueueTest done

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -1,8 +1,5 @@
 package edu.cmu.cs.cs214.rec02;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,20 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
- * TODO: 
- * 1. The {@link LinkedIntQueue} has no bugs. We've provided you with some example test cases.
- * Write your own unit tests to test against IntQueue interface with specification testing method 
- * using mQueue = new LinkedIntQueue();
- * 
- * 2. 
- * Comment `mQueue = new LinkedIntQueue();` and uncomment `mQueue = new ArrayIntQueue();`
- * Use your test cases from part 1 to test ArrayIntQueue and find bugs in the {@link ArrayIntQueue} class
- * Write more unit tests to test the implementation of ArrayIntQueue, with structural testing method
- * Aim to achieve 100% line coverage for ArrayIntQueue
+ * TODO: 1. The {@link LinkedIntQueue} has no bugs. We've provided you with some
+ * example test cases. Write your own unit tests to test against IntQueue
+ * interface with specification testing method using mQueue = new
+ * LinkedIntQueue();
+ *
+ * 2. Comment `mQueue = new LinkedIntQueue();` and uncomment `mQueue = new
+ * ArrayIntQueue();` Use your test cases from part 1 to test ArrayIntQueue and
+ * find bugs in the {@link ArrayIntQueue} class Write more unit tests to test
+ * the implementation of ArrayIntQueue, with structural testing method Aim to
+ * achieve 100% line coverage for ArrayIntQueue
  *
  * @author Alex Lockwood, George Guo, Terry Li
  */
@@ -39,7 +39,7 @@ public class IntQueueTest {
     public void setUp() {
         // comment/uncomment these lines to test each class
         mQueue = new LinkedIntQueue();
-    //    mQueue = new ArrayIntQueue();
+        //    mQueue = new ArrayIntQueue();
 
         testList = new ArrayList<>(List.of(1, 2, 3));
     }
@@ -52,20 +52,23 @@ public class IntQueueTest {
 
     @Test
     public void testNotEmpty() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+
+        mQueue.enqueue(4);
+        assertTrue(!mQueue.isEmpty());
     }
 
     @Test
     public void testPeekEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+
+        mQueue.enqueue(4);
+        int head = mQueue.peek();
+        assertEquals(head, 4);
     }
 
     @Test
     public void testPeekNoEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+
+        assertNull(mQueue.peek());
     }
 
     @Test
@@ -81,7 +84,14 @@ public class IntQueueTest {
     @Test
     public void testDequeue() {
         // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(1);
+        mQueue.enqueue(2);
+
+        assertEquals(1, mQueue.dequeue().intValue());
+        assertEquals(2, mQueue.dequeue().intValue());
+
+        assertNull(mQueue.dequeue());
+
     }
 
     @Test
@@ -104,6 +114,5 @@ public class IntQueueTest {
             }
         }
     }
-
 
 }
