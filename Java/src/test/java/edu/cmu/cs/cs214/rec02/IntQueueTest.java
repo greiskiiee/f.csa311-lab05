8ace396bad69c977b4b27c263311fbeeddd70956
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-03-14 16:20:51 +0800

    fail shalgah

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
@@ -48,14 +47,14 @@ public class IntQueueTest {
     @Test
     public void testIsEmpty() {
         // This is an example unit test
-        assertFalse(mQueue.isEmpty());
+        assertTrue(mQueue.isEmpty());
     }
 
     @Test
     public void testNotEmpty() {
 
         mQueue.enqueue(4);
-        assertFalse(mQueue.isEmpty());
+        assertTrue(mQueue.isEmpty());
     }
 
     @Test
